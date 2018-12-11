4d8672daa10a59f85021dd6dbe1401aec306cb50
roberto.davia <roberto.davia2@unibo.it>
2018-12-11 14:24:53 +0100

    Added class for automatic problem map filling

---
+++ solvers/EqMapFiller/EquationMapFiller.h
@@ -0,0 +1,30 @@
+#ifndef __EquationMapFiller_h__
+#define __EquationMapFiller_h__
+
+#include <vector>
+
+class FEMUS;
+class EquationSystemsExtendedM;
+
+enum FIELDS : int;
+
+class EquationMapFiller{  
+    
+public:    
+    int _nvars[3];
+    
+    EquationMapFiller(){}
+    ~EquationMapFiller(){}
+    void FillEquationMap(FEMUS & FemusProblem, const std::vector<FIELDS> & pbName);
+    
+    void setNavierStokes(EquationSystemsExtendedM & EqMap);
+    void setTemperature(EquationSystemsExtendedM & EqMap);
+    void setDynamicTurbulence(EquationSystemsExtendedM & EqMap);
+    void setThermalTurbulence(EquationSystemsExtendedM & EqMap);
+    void setFluidStructure(EquationSystemsExtendedM & EqMap);
+    void setColor(EquationSystemsExtendedM & EqMap);
+    void setDisplacements(EquationSystemsExtendedM & EqMap);
+    
+};
+
+#endif
